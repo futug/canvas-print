cc90ba2e7ff1e6b1807fd3ac9a0546981c127148
futug <102650930+futug@users.noreply.github.com>
2023-10-31 20:32:33 +0600

    calc frames

--- src/js/components/calcLogic.js
+++ src/js/components/calcLogic.js
@@ -4,13 +4,30 @@ import { updateSummry } from "./calcformShipment";
 const sizes = document.querySelectorAll("input[name='size']");
 const design = document.querySelector("select[name='design']");
 const coverages = document.querySelectorAll("input[name='coverage']");
+const frames = document.querySelectorAll("input[name='frame']");
+
 let basePrice = 0;
 let square = 0;
+let perimetr = 0;
 let selectedDesign = 0;
 let selectedCoverages = 0;
+let selectedFrame = 0;
 let coveragesPrice = square * selectedCoverages;
+let framePrice = perimetr * selectedFrame;
 
-export let total = basePrice + selectedDesign + coveragesPrice;
+export let total = basePrice + selectedDesign + coveragesPrice + framePrice;
+
+frames.forEach((item) => {
+  item.addEventListener("change", (e) => {
+    selectedFrame =
+      parseFloat(e.target.dataset.price) > 0
+        ? parseFloat(e.target.dataset.price) + 350
+        : 0;
+    updateTotal();
+    totalRow.textContent = ` ${total} руб.`;
+    updateSummry();
+  });
+});
 
 coverages.forEach((item) => {
   item.addEventListener("change", (e) => {
@@ -34,6 +51,7 @@ sizes.forEach((item) => {
   item.addEventListener("change", (e) => {
     const value = e.target.value;
     square = calculateSquare(value);
+    perimetr = calculatePerimetr(value);
     basePrice = parseFloat(e.target.dataset.price);
     updateTotal();
     totalRow.textContent = ` ${total} руб.`;
@@ -47,5 +65,15 @@ function calculateSquare(value) {
 }
 
 function updateTotal() {
-  total = basePrice + selectedDesign + square * selectedCoverages;
+  total = Math.ceil(
+    basePrice +
+      selectedDesign +
+      square * selectedCoverages +
+      perimetr * selectedFrame
+  );
+}
+
+function calculatePerimetr(value) {
+  const [firstNumber, secondNumber] = value.split("x");
+  return parseFloat(firstNumber) / 100 + (parseFloat(secondNumber) / 100) * 2;
 }
