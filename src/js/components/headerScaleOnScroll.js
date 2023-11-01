0b0c196bed34513c39b5c264a4504f9dd96bb43a
futug <102650930+futug@users.noreply.github.com>
2023-11-01 20:14:05 +0600

    interaction fix

--- src/js/components/headerScaleOnScroll.js
+++ src/js/components/headerScaleOnScroll.js
@@ -2,16 +2,19 @@ const header = document.querySelector(".header");
 const logo = document.querySelector(".header__logo");
 
 document.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    scaleOnScroll();
-  } else {
-    header.style.paddingTop = "20px";
-    header.style.paddingBottom = "20px";
-    logo.style.transform = "scale(1)";
+  if (window.innerWidth > 768) {
+    if (window.scrollY > 50) {
+      scaleOnScroll();
+    } else {
+      header.style.paddingTop = "20px";
+      header.style.paddingBottom = "20px";
+      logo.style.transform = "scale(1)";
+    }
   }
 });
+
 function scaleOnScroll() {
-  header.style.paddingTop = "00px";
-  header.style.paddingBottom = "00px";
+  header.style.paddingTop = "0px";
+  header.style.paddingBottom = "0px";
   logo.style.transform = "scale(0.5)";
 }
