0462a619c77b013ef51cfd39cb2d3bfe7dbe7d36
futug <102650930+futug@users.noreply.github.com>
2023-11-02 16:25:41 +0600

    tranfer value class, inputMask

---
+++ src/js/components/validation.js
@@ -0,0 +1,63 @@
+import { validateForms } from "../functions/validate-forms";
+
+const rulesPopup = [
+  {
+    ruleSelector: ".input-name",
+    rules: [
+      {
+        rule: "minLength",
+        value: 3,
+      },
+      {
+        rule: "required",
+        value: true,
+        errorMessage: "Заполните имя!",
+      },
+    ],
+  },
+  {
+    ruleSelector: ".input-tel",
+    tel: true,
+    telError: "Введите корректный телефон",
+    rules: [
+      {
+        rule: "required",
+        value: true,
+        errorMessage: "Заполните телефон!",
+      },
+    ],
+  },
+];
+
+validateForms(".pop-up__form", rulesPopup);
+
+const rulesCalcform = [
+  {
+    ruleSelector: ".calcform__form-name",
+    rules: [
+      {
+        rule: "minLength",
+        value: 3,
+      },
+      {
+        rule: "required",
+        value: true,
+        errorMessage: "Заполните имя!",
+      },
+    ],
+  },
+  {
+    ruleSelector: ".calcform__form-phone",
+    tel: true,
+    telError: "Введите корректный телефон",
+    rules: [
+      {
+        rule: "required",
+        value: true,
+        errorMessage: "Заполните телефон!",
+      },
+    ],
+  },
+];
+
+validateForms(".calcform__form", rulesCalcform);
--- src/js/components/valuesTransfer.js
+++ src/js/components/valuesTransfer.js
@@ -1,73 +1,33 @@
 export class FormTransfer {
   constructor(targetClass, formData) {
-    console.log("Полученый объект:", formData);
-    this.orientation = document.querySelector(`${targetClass}__orientation`);
-    this.size = document.querySelector(`${targetClass}__size`);
-    this.design = document.querySelector(`${targetClass}__design`);
-    this.designAdd = document.querySelector(`${targetClass}__additional`);
-    this.coverages = document.querySelector(`${targetClass}__coverage`);
-    this.frame = document.querySelector(`${targetClass}__frame`);
-    this.stretch = document.querySelector(`${targetClass}__stretch`);
-    this.suspend = document.querySelector(`${targetClass}__suspend`);
-    this.wrap = document.querySelector(`${targetClass}__wrap`);
-    this.file = document.querySelector(`${targetClass}__file`);
-    this.total = document.querySelector(`${targetClass}__total`);
-    this.name = document.querySelector(`${targetClass}__name`);
-    this.phone = document.querySelector(`${targetClass}__phone`);
-    this.email = document.querySelector(`${targetClass}__email`);
-    this.deliveryWay = document.querySelector(`${targetClass}__delivery-way`);
+    this.targetClass = targetClass;
     this.formData = formData;
+    this.fields = this.extractFields();
 
     this.transferValues(this.formData);
   }
 
+  extractFields() {
+    const elements = document.querySelectorAll(
+      `${this.targetClass} input, ${this.targetClass} select`
+    );
+    const fields = Array.from(elements).map((element) => {
+      const classList = element.classList;
+      for (let i = 0; i < classList.length; i++) {
+        if (classList[i].includes("__")) {
+          return classList[i].split("__")[1];
+        }
+      }
+    });
+    return fields.filter((field) => field);
+  }
+
   transferValues(formData) {
-    if (this.orientation) {
-      this.orientation.value = formData[0].orientation;
-    }
-    if (this.size) {
-      this.size.value = formData[0].size;
-    }
-    if (this.design) {
-      this.design.value = formData[0].design;
-    }
-
-    if (this.designAdd) {
-      this.designAdd.value = formData[0].designAdd;
-    }
-    if (this.coverages) {
-      this.coverages.value = formData[0].coverages;
-    }
-    if (this.frame) {
-      this.frame.value = formData[0].frame;
-    }
-    if (this.stretch) {
-      this.stretch.value = formData[0].stretch;
-    }
-    if (this.suspend) {
-      this.suspend.value = formData[0].suspend;
-    }
-    if (this.wrap) {
-      this.wrap.value = formData[0].wrap;
-    }
-    if (this.total) {
-      this.total.value = formData[0].total;
-    }
-
-    if (this.name) {
-      this.name.value = formData[0].name;
-    }
-
-    if (this.phone) {
-      this.phone.value = formData[0].phone;
-    }
-
-    if (this.email) {
-      this.email.value = formData[0].email;
-    }
-
-    if (this.deliveryWay) {
-      this.deliveryWay.value = formData[0].deliveryWay;
-    }
+    this.fields.forEach((field) => {
+      const element = document.querySelector(`${this.targetClass}__${field}`);
+      if (element) {
+        element.value = formData[field] || "";
+      }
+    });
   }
 }
